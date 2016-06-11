c43f6f5e40e23ef5db4c29aa6bf9373b97904554
Helmut Irle <helmut@devigninnovation.com>
2016-06-11 21:41:09 +0200

    Basics to get things going. Capture basic details.

--- gty/gty/spiders/gty_spider.py
+++ gty/gty/spiders/gty_spider.py
@@ -8,6 +8,10 @@ class GtySpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for sel in response.xpath('//table[@id="ctl00_ctl00_MainContent_SubMain_gvTitles"]/tr[not(@class="paging")]/td/li'):
+	    title = sel.xpath('h5/a/text()').extract()
+            date_preached = sel.xpath('p[1]/strong[@class="title"]/text()').extract()
+            scripture = sel.xpath('p[1]/strong[@class="date"]/text()').extract()
+            ref = sel.xpath('p[1]/text()').extract()
+            link = sel.xpath('p[2]/strong[@class="date"]/a/@href').extract()
+            print("%s - %s - %s - %s - %s" % (title, date_preached, scripture, ref, link))
